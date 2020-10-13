6bbfbcd4015e76278fedd2fbfa0e73d6f6235f9a
Damiao <tower_gamer@hotmail.com>
2020-10-13 19:04:25 -0300

    Create database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/users', (req, res) => {
     return res.json({ok:true})
 })
 
-app.listen(3333);
+app.listen(4444);
