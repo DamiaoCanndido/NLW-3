ea87a475d15c0813987496e3a3b99456aa0acb46
Damiao <tower_gamer@hotmail.com>
2020-10-16 19:17:42 -0300

    Listing one orphanage

--- web/src/pages/orphanagesMap.tsx
+++ web/src/pages/orphanagesMap.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../services/api';
 import { FiPlus, FiArrowRight } from 'react-icons/fi'; 
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
@@ -7,7 +8,7 @@ import mapMarker from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
 import MapIcon from '../utils/mapIcon';
-import api from '../services/api';
+
 
 interface Orphanage {
     id: number;
@@ -20,12 +21,10 @@ function OrphanagesMap() {
 
     const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
 
-    console.log(orphanages);
-
+    console.log(orphanages)
 
     useEffect(() => {
         api.get('orphanages').then(res => {
-            console.log(res.data);
             setOrphanages(res.data);
         })
     }, []);
@@ -50,14 +49,14 @@ function OrphanagesMap() {
                 zoom={15}
                 style={{ width: '100%', height: '100%' }}
             >
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
-                {/*<TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>*/}
+                {/*<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>*/}
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
                 {orphanages.map(orphanage => {
                     return (
                         <Marker 
+                            key={orphanage.id}
                             icon={MapIcon}
                             position={[orphanage.latitude, orphanage.longitude]}
-                            key={orphanage.id}
                         >
 
                             <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
